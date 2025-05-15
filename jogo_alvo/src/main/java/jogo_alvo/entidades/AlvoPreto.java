066f06697ecc21d38bd749d9d98cd20ea1e17deb
Lucas Léllis <125090781+Lucas-Lellis@users.noreply.github.com>
2025-05-15 15:29:37 -0300

    Update AlvoPreto.java

--- jogo_alvo/src/main/java/jogo_alvo/entidades/AlvoPreto.java
+++ jogo_alvo/src/main/java/jogo_alvo/entidades/AlvoPreto.java
@@ -1,4 +1,22 @@
 package jogo_alvo.entidades;
 
-public class AlvoPreto {
+public class AlvoPreto extends Alvo{
+
+    public AlvoPreto(Integer posX, Integer posY) {
+        super(posX, posY);
+    }
+
+    @Override
+    public boolean acerta(int posX, int posY) {
+        if (getPosX() == posX && getPosY() == posY) {
+            setQuantADestuir(getQuantADestuir() - 1);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "ALVO PRETO na posição (" + getPosY() + ", " + getPosX() + ") ";
+    }
 }
