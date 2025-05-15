e423103cd42b24f5d0145a2989520a8291609bde
Lucas Léllis <125090781+Lucas-Lellis@users.noreply.github.com>
2025-05-15 15:24:54 -0300

    Update AltoBranco.java

--- jogo_alvo/src/main/java/jogo_alvo/entidades/AltoBranco.java
+++ jogo_alvo/src/main/java/jogo_alvo/entidades/AltoBranco.java
@@ -1,5 +1,22 @@
 package jogo_alvo.entidades;
 
-public class AltoBranco {
+public class AltoBranco extends Alvo{
 
+    public AltoBranco(Integer posX, Integer posY) {
+        super(posX, posY);
+    }
+
+    @Override
+    public boolean acerta(int posX, int posY) {
+        if (getPosX() == posX && getPosY() == posY) {
+            setQuantADestuir(getQuantADestuir() - 1);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "ALVO BRANCO na posição (" + getPosX() + ", " + getPosY() + ") ";
+    }
 }
